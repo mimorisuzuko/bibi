e68b00d702cdad4e76204766dbb5becc0dda7d67
mimorisuzuko <hrky0206@gmail.com>
2025-07-28 22:14:23 +0900

    Add useCallback

--- src/useForm/index.ts
+++ src/useForm/index.ts
@@ -1,5 +1,5 @@
 import { toMerged } from "es-toolkit";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import type { PartialDeep } from "type-fest";
 import type { z } from "zod";
 
@@ -8,63 +8,77 @@ export const useForm = <T extends z.ZodObject>(args: {
 	initialValues: z.infer<T>;
 }) => {
 	const [formState, _setFormState] = useState(args.initialValues);
-	const setFormState = (patches: PartialDeep<typeof formState>) => {
+	const setFormState = useCallback((patches: PartialDeep<typeof formState>) => {
 		_setFormState((previous) => toMerged(previous, patches));
-	};
-	const setValues = (patches: Partial<typeof formState>) => {
+	}, []);
+	const setValues = useCallback((patches: Partial<typeof formState>) => {
 		_setFormState((previous) => ({ ...previous, ...patches }));
-	};
+	}, []);
 
 	const [immediatelyValidatedKeys, setImmediatelyValidatedKeys] = useState<
 		string[]
 	>([]);
-	const setImmediatelyValidatedKey = (key: string, push = true) => {
-		const index = immediatelyValidatedKeys.indexOf(key);
+	const setImmediatelyValidatedKey = useCallback(
+		(key: string, push = true) => {
+			const index = immediatelyValidatedKeys.indexOf(key);
 
-		if (push && index === -1) {
-			setImmediatelyValidatedKeys((previous) => [...previous, key]);
-		} else if (!push && index >= -1) {
-			setImmediatelyValidatedKeys(immediatelyValidatedKeys.toSpliced(index, 1));
-		}
-	};
+			if (push && index === -1) {
+				setImmediatelyValidatedKeys((previous) => [...previous, key]);
+			} else if (!push && index >= -1) {
+				setImmediatelyValidatedKeys(
+					immediatelyValidatedKeys.toSpliced(index, 1)
+				);
+			}
+		},
+		[immediatelyValidatedKeys]
+	);
 
-	const getErrors = (specifiedKeys?: string[]) => {
-		const parsed = args.schema.safeParse(formState);
+	const _getErrors = useCallback(
+		(specifiedKeys?: string[]) => {
+			const parsed = args.schema.safeParse(formState);
 
-		if (parsed && !parsed.success) {
-			const entries: [key: string, message: string][] = [];
+			if (parsed && !parsed.success) {
+				const entries: [key: string, message: string][] = [];
 
-			for (const { path, message } of parsed.error.issues) {
-				const key = path.join(".");
+				for (const { path, message } of parsed.error.issues) {
+					const key = path.join(".");
 
-				if (specifiedKeys === undefined || specifiedKeys.includes(key)) {
-					entries.push([path.join("."), message]);
+					if (specifiedKeys === undefined || specifiedKeys.includes(key)) {
+						entries.push([path.join("."), message]);
+					}
 				}
+
+				return entries.length === 0 ? undefined : Object.fromEntries(entries);
 			}
 
-			return entries.length === 0 ? undefined : Object.fromEntries(entries);
-		}
+			return undefined;
+		},
+		[args.schema, formState]
+	);
 
-		return undefined;
-	};
+	const [_hasSubmitted, _setHasSubmitted] = useState(false);
+	const createOnSubmit = useCallback(
+		(f: (valid: boolean) => void) => {
+			return (e?: any) => {
+				if (e instanceof Event) {
+					e.preventDefault();
+				}
 
-	const [hasSubmitted, setHasSubmitted] = useState(false);
-	const createOnSubmit = (f: (valid: boolean) => void) => {
-		return (e?: any) => {
-			if (e instanceof Event) {
-				e.preventDefault();
-			}
-
-			f(getErrors() === undefined);
-			setHasSubmitted(true);
-		};
-	};
-	const errors = hasSubmitted
-		? getErrors()
-		: getErrors(immediatelyValidatedKeys);
-	const getError = (key: string) => {
-		return errors ? errors[key] : undefined;
-	};
+				f(_getErrors() === undefined);
+				_setHasSubmitted(true);
+			};
+		},
+		[_getErrors]
+	);
+	const errors = _hasSubmitted
+		? _getErrors()
+		: _getErrors(immediatelyValidatedKeys);
+	const getError = useCallback(
+		(key: string) => {
+			return errors ? errors[key] : undefined;
+		},
+		[errors]
+	);
 
 	const valid = errors === undefined;
 
